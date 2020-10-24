60f8b551ebb901f2895b891dd71b8d1f1c6a95a7
Jamester <rodriang62@gmail.com>
2020-10-24 03:18:07 -0400

    Agregado script de start al backend. y algunos comentarios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const app = require('./app');
 require('./database');
+
 // esto se ve mucho en java y en C#
 async function  main(){
    await app.listen(app.get('port'));
    console.log("Server on port" , app.get('port'));
 }
-
+// ejecucion de la funcion principal
 main();
 
 
